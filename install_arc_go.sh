f27b74a2dd498e5ce3ec39dc236c050d226ab9eb
Khushi chhillar <120015301+kcl17@users.noreply.github.com>
2024-02-22 12:02:47 +0530

    Create install_arc_go.sh

--- install_arc_go.sh
+++ install_arc_go.sh
@@ -2,29 +2,47 @@
 
 # Function to display script usage
 usage() {
-    echo "Usage: $0 -v <go_version>"
+    echo "Usage: $0 -v <go_version> -a <architecture>"
+    echo "Supported architectures: amd64, arm64, armv6l, armv7l, ppc64le, s390x, x86_64"
     exit 1
 }
 
 # Function to determine system architecture
 get_architecture() {
-    arch=$(uname -m)
-    case $arch in
-        x86_64)
+    case $1 in
+        amd64|x86_64)
             echo "amd64"
             ;;
+        arm64)
+            echo "arm64"
+            ;;
+        armv6l)
+            echo "armv6l"
+            ;;
+        armv7l)
+            echo "armv7l"
+            ;;
+        ppc64le)
+            echo "ppc64le"
+            ;;
+        s390x)
+            echo "s390x"
+            ;;
         *)
-            echo "Unsupported architecture: $arch"
+            echo "Unsupported architecture: $1"
             exit 1
             ;;
     esac
 }
 
 # Parse command line options
-while getopts ":v:" opt; do
+while getopts ":v:a:" opt; do
     case $opt in
         v)
             GO_VERSION="$OPTARG"
+            ;;
+        a)
+            ARCH="$OPTARG"
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -37,14 +55,14 @@ while getopts ":v:" opt; do
     esac
 done
 
-# Verify that the version is provided
-if [ -z "$GO_VERSION" ]; then
-    echo "Error: Go version not provided."
+# Verify that both version and architecture are provided
+if [ -z "$GO_VERSION" ] || [ -z "$ARCH" ]; then
+    echo "Error: Go version and architecture must be provided."
     usage
 fi
 
 # Determine system architecture
-ARCH=$(get_architecture)
+ARCH=$(get_architecture "$ARCH")
 
 # Specify the installation directory (change as needed)
 INSTALL_DIR="/usr/local"
